74ab3242fa1cb5b8366ab9a6e3c51342450bf989
kashani366 <66195439+kashani366@users.noreply.github.com>
2020-06-18 12:58:33 -0700

    update

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -2,32 +2,76 @@ package model;
 
 import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
+
 public class Admin {
     Scanner sc=new Scanner(System.in);
-
+//boolean y=true;
     private String emailAdmin="kashani_sara@yahoo.com";
     private String passwordAdmin="123";
     public Admin() {
 
     }
-public  void checkAdmin(String input){
+public  void checkAdmin(String input) {
     System.out.println("enter email");
-    String input1=sc.nextLine();
-    if(input1=="kashani_sara@yahoo.com")
-        System.out.println("enter password");
-String input2=sc.nextLine();
-if(input2=="123"){
-    showmenue();
-}//else
+    String input1 = sc.next();
+    System.out.println("enter password");
+    String input2 = sc.next();
+
+    if (input1.equals("sara") && input2.equals("123")) {
+        System.out.println("your email is correct");
+        System.out.println("your password is coorect");
+
+        showmenue();
+
+
+    }//else
 //break;
 }
 
     private void showmenue() {
+        System.out.println("pleas enter youe choose:");
+      //  sleep(1000);
         System.out.println("1.Add librarian");
         System.out.println("2.view librarian");
         System.out.println("3.edit");
-        System.out.println("delet");
-        System.out.println("logout");
+        System.out.println("4delet");
+        System.out.println("5logout");
+       int  input3= sc.nextInt();
+        routInput(input3);
+
+    }
+
+    private void routInput(int input3) {
+        switch (input3){
+            case 1:
+                addLibrarian();
+                break;
+            case 2:
+                showlist();
+                break;
+            case 3:
+                editlist();
+                break;
+            case 4:
+                delet();
+                break;
+            case 5:
+                logout();
+                break;
+
+        }
+    }
+
+    private void addLibrarian() {
+    }
+    private void showlist() {
+    }
+    private void editlist() {
+    }
+    private void delet() {
+    }
+    private void logout() {
     }
 }
 
--- src/model/main.java
+++ src/model/main.java
@@ -5,26 +5,32 @@ import java.util.Scanner;
 
 public class main {
   public static   Admin admin=new Admin();
+  boolean isAlive=true;
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         ArrayList<librarian> librarians=new ArrayList<librarian>();
-        System.out.println("--welcom to elibrary --(type/help to start");
-        System.out.println("are you admin?");
+        System.out.println("--welcom to elibrary --(type/help to start)");
+        System.out.println("are you admin?y/n");
+        String input=sc.nextLine();
+boolean y=true;
+boolean n=true;
         //aplication loop
-        while (true){
+        while (y){
             //get input
-            String input=sc.nextLine();
+            //String input=sc.nextLine();
             //process
             //show result(feeadback)
+            admin.checkAdmin(input);
+y=false;
 
         }
         //application quit
       //  System.out.println("the end");
     }
     public static  void  routInput(String input){
-        if(input=="y"){
-admin.checkAdmin(input);
+        while(input=="n"){
+
         }
     }
 }
